6807157e8c2c9a1573b5f02df1827824feae097c
mchang35 <mandy_chang@brown.edu>
2022-08-29 17:40:19 -0500

    Day 1 done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 11-11
 Repository to host the webpages for the 11:11 game website, inspired by Xue.
+
+Some helpful resources include:
+ * [Creating a stopwatch - HTML, CSS, Javascript](https://blog.devgenius.io/creating-a-stopwatch-timer-with-html-css-and-javascript-d97da2f23554)
+ * [Adding an event listener using addEventListener - Javascript](https://www.w3schools.com/jsref/met_element_addeventlistener.asp)
+ * [Rounded buttons: border-radius - CSS](https://www.w3schools.com/howto/howto_css_round_buttons.asp)
+ * 
--- main.js
+++ main.js
@@ -1,1 +1,78 @@
+var start_str = "START";
+var stop_str = "STOP";
+var timer_on = false;
 
+function button_pressed() {
+    button = document.getElementById("button");
+    console.log("Button's text:" + button.innerHTML);
+    if (button.innerHTML == start_str) {
+        start(button);
+    } else {
+        stop();
+    }
+}
+
+function start(button) {
+    console.log("start was pressed");
+    // change the button to be a stop button
+    button.innerHTML = stop_str;
+    button.classList.remove("start");
+    button.classList.add("stop");
+
+    second = document.getElementById("second");
+    millisecond = document.getElementById("millisecond");
+
+    // start the timer
+    timer_on = true;
+    let sec = 0;
+    let ms = 0;
+    while (timer_on) {
+        setTimeout(function() {
+            ms += 1;
+            if (ms >= 1000) {
+                ms = 0;
+                sec += 1
+            }
+            if (sec >= 60) {
+                timer_on = false;
+                time_out();
+            }
+            // display the times
+            sec_str = String(sec);
+            if (len(sec_str) == 1) {
+                sec_str = "0" + sec_str;
+            }
+
+            ms_str = String(ms);
+            if (len(ms_str) == 1) {
+                ms_str = "00" + ms_str;
+            } else if (len(ms_str) == 2) {
+                ms_str = "0" + ms_str;
+            }
+
+            second.innerHTML = sec_str;
+            millisecond.innerHTML = ms_str;
+        }, 1); // wait 1 ms between runs
+
+    }
+}
+
+function time_out() {
+    // pop up message that says, "you've timed out."
+}
+
+function stop() {
+    console.log("stop was pressed");
+    // stop the timer
+    timer_on = false;
+
+    // wait 2 seconds
+    
+    // if win:
+
+    // else (if lose): pop up with Try Again button
+}
+
+function try_again() {
+
+}
